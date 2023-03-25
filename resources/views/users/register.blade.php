8adc32c1ff9846a87cd7d61ef70c459a1f763786
mjmj112233 <palermarcusj@gmail.com>
2023-03-25 20:11:58 +0800

    register now working XD

--- resources/views/users/register.blade.php
+++ resources/views/users/register.blade.php
@@ -42,14 +42,14 @@
         .brand {
             position: relative;
             top: 310px;
-            left: -20px;
+            left: -320px;
             color: white;
-            z-index: 9999;
+            z-index: 999;
         }
 
         .title {
             font-weight: bold;
-            margin-left: 20rem;
+            margin-left: 38rem;
             font-size: 50px;
         }
 
@@ -59,22 +59,23 @@
             text-align: center;
         }
 
-        .form-container {
+        #form-container {
             float: right;
             position: relative;
+            top: -130px;
             right: 90px;
             background: white;
             border-radius: 25px;
             width: 40%;
-        }
-
-        .form-header {
-            text-align: center;
-            margin-top: 50px;
+            z-index: 9999;
         }
 
         .header1,
         .header2 {
+            position: relative;
+            top: 50px;
+            left: 98px;
+            text-align: center;
             display: inline;
             font-size: 45px;
         }
@@ -111,148 +112,144 @@
 
 </head>
 
+<img id="blink_ec" src="{{url('/images/blink_ec.png')}}">
 
-<form method="POST" action="/users">
+<div class="temp"></div>
+
+<div class="brand">
+    <h1 class="title">annyeong!</h1>
+    <h1 class="subtitle">Step into a K-pop lover's paradise with annyeong! <br> Your ultimate online destination for the hottest K-pop merch and more!</h1>
+</div>
+
+<form method="POST" id="form-container" action="/users">
     @csrf
 
-    <img id="blink_ec" src="{{url('/images/blink_ec.png')}}">
+    <h1 class=header1>Sign up</h1>
+    <h1 class=header2>Log in</h1>
+   
+    <div class="mb-2">
+        <label for="fname" class="inline-block text-lg mb-2">
+        </label>
+        <input type="text" class="border-[3px] border-pink-900 rounded-[50px] bg-input placeholder-gray-600 text-center ml-16 mb-2 mt-24 p-2 w-9/12" name="fname" value="{{old('fname')}}" placeholder="First Name" />
 
-    <div class="temp"></div>
+        @error('fname')
+        <p class="text-red-500 text-xs mt-1 ml-20">{{$message}}</p>
+        @enderror
+    </div>
+    
+    <div class="mb-2">
+        <label for="lname" class="inline-block text-lg mb-2">
+        </label>
+        <input type="text" class="border-[3px] border-pink-900 rounded-[50px] bg-input placeholder-gray-600 text-center ml-16 mt-5 p-2 w-9/12" name="lname" value="{{old('lname')}}" placeholder="Last Name" />
 
-    <div class="form-container">
-
-        <div class="form-header">
-            <h1 class=header1>Sign up</h1>
-            <h1 class=header2>Log in</h1>
-        </div>
-
-
-        <div class="mb-2">
-            <label for="fname" class="inline-block text-lg mb-2">
-            </label>
-            <input type="text" class="border-[3px] border-pink-900 rounded-[50px] bg-input placeholder-gray-600 text-center ml-16 mt-20 p-2 w-9/12" name="fname" value="{{old('fname')}}" placeholder="First Name" />
-
-            @error('fname')
-            <p class="text-red-500 text-xs mt-1 ml-20">{{$message}}</p>
-            @enderror
-        </div>
-
-        <div class="mb-2">
-            <label for="lname" class="inline-block text-lg mb-2">
-            </label>
-            <input type="text" class="border-[3px] border-pink-900 rounded-[50px] bg-input placeholder-gray-600 text-center ml-16 mt-5 p-2 w-9/12" name="lname" value="{{old('lname')}}" placeholder="Last Name" />
-
-            @error('lname')
-            <p class="text-red-500 text-xs mt-1 ml-20">{{$message}}</p>
-            @enderror
-        </div>
-
-        <div class="mb-2">
-            <label for="uname" class="inline-block text-lg mb-2">
-            </label>
-            <input type="text" class="border-[3px] border-pink-900 rounded-[50px] bg-input placeholder-gray-600 text-center ml-16 mt-5 p-2 w-9/12" name="uname" value="{{old('uname')}}" placeholder="Username" />
-
-            @error('uname')
-            <p class="text-red-500 text-xs mt-1 ml-20">{{$message}}</p>
-            @enderror
-        </div>
-
-        <div class="mb-2">
-            <label for="birthday" class="inline-block text-lg mb-2">
-            </label>
-            <input type="text" class="border-[3px] border-pink-900 rounded-[50px] bg-input placeholder-gray-600 text-center ml-16 mt-20 p-2 w-9/12" name="birthday" value="{{old('birthday')}}" placeholder="Birthday (YYYY-MM-DD)" />
-
-            @error('birthday')
-            <p class="text-red-500 text-xs mt-1 ml-20">{{$message}}</p>
-            @enderror
-        </div>
-
-        <div class="mb-2">
-            <label for="contact" class="inline-block text-lg mb-2">
-            </label>
-            <input type="text" class="border-[3px] border-pink-900 rounded-[50px] bg-input placeholder-gray-600 text-center ml-16 mt-5 p-2 w-9/12" name="contact" value="{{old('contact')}}" placeholder="Contact No." />
-
-            @error('contact')
-            <p class="text-red-500 text-xs mt-1 ml-20">{{$message}}</p>
-            @enderror
-        </div>
-
-        <div class="mb-2">
-            <label for="street" class="inline-block text-lg mb-2">
-            </label>
-            <input type="text" class="border-[3px] border-pink-900 rounded-[50px] bg-input placeholder-gray-600 text-center ml-16 mt-5 p-2 w-9/12" name="street" value="{{old('street')}}" placeholder="Street" />
-
-            @error('street')
-            <p class="text-red-500 text-xs mt-1 ml-20">{{$message}}</p>
-            @enderror
-        </div>
-
-        <div class="mb-2">
-            <label for="city" class="inline-block text-lg mb-2">
-            </label>
-            <input type="text" class="border-[3px] border-pink-900 rounded-[50px] bg-input placeholder-gray-600 text-center ml-16 mt-5 p-2 w-9/12" name="city" value="{{old('city')}}" placeholder="City" />
-
-            @error('city')
-            <p class="text-red-500 text-xs mt-1 ml-20">{{$message}}</p>
-            @enderror
-        </div>
-
-        <div class="mb-2">
-            <label for="zip" class="inline-block text-lg mb-2">
-            </label>
-            <input type="text" class="border-[3px] border-pink-900 rounded-[50px] bg-input placeholder-gray-600 text-center ml-16 mt-5 p-2 w-9/12" name="zip" value="{{old('zip')}}" placeholder="ZIP" />
-
-            @error('zip')
-            <p class="text-red-500 text-xs mt-1 ml-20">{{$message}}</p>
-            @enderror
-        </div>
-
-        <div class="mb-2">
-            <label for="email" class="inline-block text-lg mb-2"></label>
-            <input type="email" class="border-[3px] border-pink-900 rounded-[50px] bg-input placeholder-gray-600 text-center ml-16 mt-20 p-2 w-9/12" name="email" value="{{old('email')}}" placeholder="Email" />
-
-            @error('email')
-            <p class="text-red-500 text-xs mt-1 ml-20">{{$message}}</p>
-            @enderror
-        </div>
-
-        <div class="mb-2">
-            <label for="password" class="inline-block text-lg mb-2">
-            </label>
-            <input type="password" class="border-[3px] border-pink-900 rounded-[50px] bg-input placeholder-gray-600 text-center ml-16 mt-5 p-2 w-9/12" name="password" value="{{old('password')}}" placeholder="Password" />
-
-            @error('password')
-            <p class="text-red-500 text-xs mt-1 ml-20">{{$message}}</p>
-            @enderror
-        </div>
-
-        <div class="mb-2">
-            <label for="password2" class="inline-block text-lg mb-2">
-            </label>
-            <input type="password" class="border-[3px] border-pink-900 rounded-[50px] bg-input placeholder-gray-600 text-center ml-16 mt-5 p-2 w-9/12" name="password_confirmation" placeholder="Confirm Password" />
-
-            @error('password_confirmation')
-            <p class="text-red-500 text-xs mt-1 ml-20">{{$message}}</p>
-            @enderror
-        </div>
-
-
-        <div class="mt-16 mb-6 text-center text-lg">
-            <button type="submit" class="bg-main text-white py-3 px-[143px] rounded-[50px] hover:bg-hover">
-                SIGNUP
-            </button>
-        </div>
-
-        <div class="mt-4 pb-20 text-center">
-            <p>
-                Already have an account?
-                <a href="/login" class="text-main">Login</a>
-            </p>
-        </div>
+        @error('lname')
+        <p class="text-red-500 text-xs mt-1 ml-20">{{$message}}</p>
+        @enderror
     </div>
 
-    <div class="brand">
-        <h1 class="title">annyeong!</h1>
-        <h1 class="subtitle">Step into a K-pop lover's paradise with annyeong! <br> Your ultimate online destination for the hottest K-pop merch and more!</h1>
+    <div class="mb-2">
+        <label for="uname" class="inline-block text-lg mb-2">
+        </label>
+        <input type="text" class="border-[3px] border-pink-900 rounded-[50px] bg-input placeholder-gray-600 text-center ml-16 mt-5 p-2 w-9/12" name="uname" value="{{old('uname')}}" placeholder="Username" />
+
+        @error('uname')
+        <p class="text-red-500 text-xs mt-1 ml-20">{{$message}}</p>
+        @enderror
     </div>
+
+    <div class="mb-2">
+        <label for="birthday" class="inline-block text-lg mb-2">
+        </label>
+        <input type="text" class="border-[3px] border-pink-900 rounded-[50px] bg-input placeholder-gray-600 text-center ml-16 mt-20 p-2 w-9/12" name="birthday" value="{{old('birthday')}}" placeholder="Birthday (YYYY-MM-DD)" />
+
+        @error('birthday')
+        <p class="text-red-500 text-xs mt-1 ml-20">{{$message}}</p>
+        @enderror
+    </div>
+
+    <div class="mb-2">
+        <label for="contact" class="inline-block text-lg mb-2">
+        </label>
+        <input type="text" class="border-[3px] border-pink-900 rounded-[50px] bg-input placeholder-gray-600 text-center ml-16 mt-5 p-2 w-9/12" name="contact" value="{{old('contact')}}" placeholder="Contact No." />
+
+        @error('contact')
+        <p class="text-red-500 text-xs mt-1 ml-20">{{$message}}</p>
+        @enderror
+    </div>
+
+    <div class="mb-2">
+        <label for="street" class="inline-block text-lg mb-2">
+        </label>
+        <input type="text" class="border-[3px] border-pink-900 rounded-[50px] bg-input placeholder-gray-600 text-center ml-16 mt-5 p-2 w-9/12" name="street" value="{{old('street')}}" placeholder="Street" />
+
+        @error('street')
+        <p class="text-red-500 text-xs mt-1 ml-20">{{$message}}</p>
+        @enderror
+    </div>
+
+    <div class="mb-2">
+        <label for="city" class="inline-block text-lg mb-2">
+        </label>
+        <input type="text" class="border-[3px] border-pink-900 rounded-[50px] bg-input placeholder-gray-600 text-center ml-16 mt-5 p-2 w-9/12" name="city" value="{{old('city')}}" placeholder="City" />
+
+        @error('city')
+        <p class="text-red-500 text-xs mt-1 ml-20">{{$message}}</p>
+        @enderror
+    </div>
+
+    <div class="mb-2">
+        <label for="zip" class="inline-block text-lg mb-2">
+        </label>
+        <input type="text" class="border-[3px] border-pink-900 rounded-[50px] bg-input placeholder-gray-600 text-center ml-16 mt-5 p-2 w-9/12" name="zip" value="{{old('zip')}}" placeholder="ZIP" />
+
+        @error('zip')
+        <p class="text-red-500 text-xs mt-1 ml-20">{{$message}}</p>
+        @enderror
+    </div>
+
+    <div class="mb-2">
+        <label for="email" class="inline-block text-lg mb-2"></label>
+        <input type="email" class="border-[3px] border-pink-900 rounded-[50px] bg-input placeholder-gray-600 text-center ml-16 mt-20 p-2 w-9/12" name="email" value="{{old('email')}}" placeholder="Email" />
+
+        @error('email')
+        <p class="text-red-500 text-xs mt-1 ml-20">{{$message}}</p>
+        @enderror
+    </div>
+
+    <div class="mb-2">
+        <label for="password" class="inline-block text-lg mb-2">
+        </label>
+        <input type="password" class="border-[3px] border-pink-900 rounded-[50px] bg-input placeholder-gray-600 text-center ml-16 mt-5 p-2 w-9/12" name="password" value="{{old('password')}}" placeholder="Password" />
+
+        @error('password')
+        <p class="text-red-500 text-xs mt-1 ml-20">{{$message}}</p>
+        @enderror
+    </div>
+
+    <div class="mb-2">
+        <label for="password2" class="inline-block text-lg mb-2">
+        </label>
+        <input type="password" class="border-[3px] border-pink-900 rounded-[50px] bg-input placeholder-gray-600 text-center ml-16 mt-5 p-2 w-9/12" name="password_confirmation" placeholder="Confirm Password" />
+
+        @error('password_confirmation')
+        <p class="text-red-500 text-xs mt-1 ml-20">{{$message}}</p>
+        @enderror
+    </div>
+
+
+    <div class="mt-16 mb-6 text-center text-lg">
+        <button type="submit" class="bg-main text-white py-3 px-[143px] rounded-[50px] hover:bg-hover">
+            SIGNUP
+        </button>
+    </div>
+
+    <div class="mt-4 pb-20 text-center">
+        <p>
+            Already have an account?
+            <a href="/login" class="text-main">Login</a>
+        </p>
+    </div>
+
+
+
 </form>
