77073eaa25eadc524b23f701b645abaae1f0d008
Litsarr <lvlascieras2201@gmail.com>
2023-04-16 12:58:58 +0800

    Update ProductsController.php

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -7,13 +7,38 @@ use Illuminate\Http\Request;
 
 class ProductsController extends Controller
 {
+
     //show all products
     public function index() {
-        return view('productsHome', [
-            'heading' => 'Our Products',
-            'products' => Product::latest()->filter
-            (request(['search'])) -> get()
+           return view('productsHome', [
+                'heading' => 'Our Products',
+                'products' => Product::latest()->filter
+                (request(['search'])) -> get()
+            ]);
+        }
+
+    //list product form
+    public function create(){
+         return view('create');
+    }
+
+    //store listed product
+    public function store(Request $request){
+        $formFields = $request -> validate([
+            'name' => 'required',
+            'price' => 'required',
+            'stock' => 'required',
+            'description' => 'required',
+            'Category_ID' => 'required',
+            'Fandom_ID' => 'required',
+            'Seller_ID' => 'required'
         ]);
+
+        Product::create($formFields);
+
+        return redirect('/');
+
+
     }
 
     //show single product
@@ -22,4 +47,5 @@ class ProductsController extends Controller
             'product' => $product
         ]);
     }
+
 }
